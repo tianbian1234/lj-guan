099e97a11489f4b32e6a39055526bf36aeb63821
gaoyantao <gaoyantao@mrocker.com>
2017-09-11 21:53:52 +0800

    修改

---
+++ src/components/nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.scss';
+import Img from 'public/images/icon.jpg';
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <nav className="nav-box">
+        <a href="../peixun/index.html"><img src={Img} alt='创客空间logo'/></a>
+        <ul className="nav-ul">
+          <li className="active"><a>首页</a></li>
+          <li><a>文章</a></li>
+          <li><a>消息</a></li>
+          <li><a>关于</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { HashRouter, hashHistory } from 'react-router-dom';
 import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<HashRouter history={hashHistory}>
+  <App>
+
+  </App>
+</HashRouter>, document.getElementById('app'));
